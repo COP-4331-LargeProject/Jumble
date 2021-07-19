4ae1ee5ff648e7a807c9817807635d1a5e19aa56
gexrgia <84422659+gexrgia@users.noreply.github.com>
2021-07-18 23:56:18 -0400

    password recovery

---
+++ password.js
@@ -0,0 +1,26 @@
+//recover password
+document.querySelector('verifyButton').addEventListener('click', () => { //match jsx
+    console.log('reset password')
+
+    const email_input = document.querySelector('email').value; //match jsx
+
+    fetch('/api/password', { //match api
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email_input, //match api
+        })
+    })
+
+        .then(res => {
+            if (res.ok) {
+                console.log('Success')
+                window.location.replace('/login') //remove if redirect is not needed
+            } else {
+                console.log('Error')
+            }
+        })
+        .catch(error => console.log('Error', error))
+})
