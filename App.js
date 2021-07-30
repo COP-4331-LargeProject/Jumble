b209bc8e2a38af6794104f04670c2c2d45cba4e8
marianaboteroq <46663848+marianaboteroq@users.noreply.github.com>
2021-07-30 15:30:25 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -10,21 +10,24 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Genre from './pages/Genre';
 import Password from './pages/Password';
 import Verification from './pages/Verification';
+import PasswordVerification from './pages/PasswordVerification';
 
 const Stack = createStackNavigator();
 
+// HEADER SHOWN SET TO FALSE SO IT DOESNT SHOW WHAT IT LOOKS LIKE
 export default function App() {
   return (
-    //<View>
+    //<View> 
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Login" screenOptions={{
-          headerShown: true,
+          headerShown: false,
   }}> 
           <Stack.Screen name="Login Screen" component={Login} />
           <Stack.Screen name="Signup Screen" component={Signup} />
           <Stack.Screen name="Home Screen" component={Home} />
           <Stack.Screen name="Genre Screen" component={Genre} />
           <Stack.Screen name="Password Screen" component={Password} />
+          <Stack.Screen name="Password Verification Screen" component={PasswordVerification} />
           <Stack.Screen name="Verify Screen" component={Verification} />
         </Stack.Navigator>
       </NavigationContainer>
