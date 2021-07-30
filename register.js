41b421276bd3634da7f2d735679d1381f76f48a0
gexrgia <84422659+gexrgia@users.noreply.github.com>
2021-07-29 22:39:58 -0400

    updated register api call

--- register.js
+++ register.js
@@ -1,12 +1,11 @@
 //register
-document.querySelector('#register-btn').addEventListener('click', () => { //match jsx
-  console.log('register')
+const email_input = document.querySelector('email').value //match frontend
+const pass_input = document.querySelector('password').value //match frontend
+const first_name_input = document.querySelector('first_name').value //match frontend
+const last_name_input = document.querySelector('last_name').value //match frontend
 
-  const email_input = document.querySelector('#email') //match jsx
-  const pass_input = document.querySelector('#password') //match jsx
-  const first_name_input = document.querySelector('#first_name') //match jsx
-  const last_name_input = document.querySelector('#last_name') //match jsx
-  const register_error = document.querySelector('#error') //optional
+async function register() {
+  if(!email_input || !pass_input || !first_name_input || !last_name_input) return
 
   fetch('/api/register', { //match api
     method: 'POST',
@@ -14,21 +13,16 @@ document.querySelector('#register-btn').addEventListener('click', () => { //matc
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      email: email_input.value, //match api
-      password: pass_input.value //match api
-      first_name: first_name_input.value, //match api
-      last_name: last_name_input.value //match api
+      email: email_input, //match api
+      password: pass_input, //match api
+      first_name: first_name_input, //match api
+      last_name: last_name_input //match api
     })
   })
 
-    .then(res => {
-      if (res.ok) {
-        console.log('Success')
-        window.location.replace('/verification')
-      } else {
-        console.log('Error')
-        login_error.style.display = 'block' //optional
-      }
-    })
-    .catch(error => console.log('Error', error))
-})
+  if(!res.ok) {
+    console.log('Error')
+  } else {
+    window.location.replace('/login') //match url
+  }
+}
