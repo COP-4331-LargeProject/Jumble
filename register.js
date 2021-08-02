92ec0d1ede99cb6a1a2a4a8187f44cca7653dc63
gexrgia <84422659+gexrgia@users.noreply.github.com>
2021-08-01 20:41:23 -0400

    final register api call

--- register.js
+++ register.js
@@ -1,22 +1,24 @@
 //register
-const email_input = document.querySelector('email').value //match frontend
-const pass_input = document.querySelector('password').value //match frontend
-const first_name_input = document.querySelector('first_name').value //match frontend
-const last_name_input = document.querySelector('last_name').value //match frontend
+const email_input = document.querySelector('email').value //matches frontend
+const pass_input = document.querySelector('password').value //matches frontend
+const confirm_pass_input = document.querySelector('confirmPassword').value //matches frontend
+const first_name_input = document.querySelector('firstName').value //matches frontend
+const last_name_input = document.querySelector('lastName').value //matches frontend
 
 async function register() {
   if(!email_input || !pass_input || !first_name_input || !last_name_input) return
+  if(pass_input != confirm_pass_input) return
 
-  const res = fetch('/api/register', { //match api
+  const res = fetch('/api/register', { //matches api
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      email: email_input, //match api
-      password: pass_input, //match api
-      first_name: first_name_input, //match api
-      last_name: last_name_input //match api
+      email: email_input, //matches api
+      password: pass_input, //matches api
+      first_name: first_name_input, //matches api
+      last_name: last_name_input //matches api
     })
   })
 
