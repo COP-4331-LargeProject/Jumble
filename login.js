131940f4b48de55fb4ab2904197088c0eea31631
gexrgia <84422659+gexrgia@users.noreply.github.com>
2021-08-02 19:56:37 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,26 +1,26 @@
 //login
-const email_input = document.querySelector('email').value; //match frontend
-const pass_input = document.querySelector('password').value; //match frontend
+const email_input = document.querySelector('email').value; //matches frontend
+const pass_input = document.querySelector('password').value; //matches frontend
 
 async function login() {
     if(!email_input || !pass_input) return
 
-    const res = await fetch('/api/login', { //match api
+    const res = await fetch('/api/login', { //matches api
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         },
         body: JSON.stringify({
-            email: email_input, //match api
-            password: pass_input //match api
+            email: email_input, //matches api
+            password: pass_input //matches api
         })
     })
 
-    if (!res.ok) {
-        console.log('Incorrect username/password')
-    } else {
+    if (res.ok) {
         console.log('Success')
         window.location.replace('/') //match url
+    } else {
+        console.log('Incorrect username/password')
     }
 }
